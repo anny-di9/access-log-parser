79951c0b59c429c138b8bea09e90c20481b77a11
Anna Selyutina <a394216@mail.ru>
2022-12-11 02:36:50 +0300

    Курсовой проект: Задание по теме "Циклы"

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
+import java.io.File;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Случайное число от 0 до 1: " + Math.random());
+        int count = 0;
+        while (true){
+            System.out.println("Введите путь файла и нажмаите Enter:");
+            String path = new Scanner(System.in).nextLine();
+
+            File file = new File(path);
+            boolean fileExists = file.exists();
+            boolean isDirectory = file.isDirectory();
+
+            if(isDirectory){
+                System.out.println("Указан путь к папке, а не к файлу");
+                continue;
+            }
+
+            if (fileExists) System.out.println("Путь указан верно, номер файла:" + count++);
+            else System.out.println("Файл не существует");
+        }
     }
 }
+
+
